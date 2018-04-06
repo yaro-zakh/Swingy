e3b87724614bea75d7cdff4e2c5f010196ab561c
yaro-zack <jaro.zack@gmail.com>
2018-04-06 23:39:43 +0300

    All work

--- src/main/java/ua/yzcorp/view/Gui.java
+++ src/main/java/ua/yzcorp/view/Gui.java
@@ -31,19 +31,20 @@ public class Gui {
 	private static JFrame frame;
 	private static JTextField nameHeroTextField = new JTextField();
 	private static String classHero;
-	private static JButton createButton = new JButton("Create");
-	private static JButton chooseButton = new JButton("Choose");
+	private static JButton createButton;
+	private static JButton chooseButton;
 	private static JTextPane textPane = new JTextPane();
 	private static JPanel panelButtonCreateAndChoose = new JPanel();
 	private static JPanel panelTableChoose = new JPanel();
 	private static JPanel panelFieldCreate = new JPanel();
 	private static ImagePanel mainPanel;
-	private static MyTableModel tableModel;
+	private static MyTableModel tableModel = null;
 	private static JTable allHeroesTable;
+	private static JScrollPane scrollPane;
 	private static StyledDocument doc;
 	private static SimpleAttributeSet center;
 	private static ImagePanel panelMap;
-	private static  CreateStartMap createMap = new CreateStartMap();
+	private static CreateStartMap createMap = new CreateStartMap();
 	private static JPanel[][] cell;
 	private static BufferedImage heroImage;
 	private static BufferedImage enemyImage;
@@ -57,22 +58,64 @@ public class Gui {
 	private static JProgressBar progressLevel = new JProgressBar();
 	private static JProgressBar progressHP = new JProgressBar();
 
+	private static ImagePanel gameOverPanel;
+	private static ImagePanel winPanel;
+	private static BufferedImage gameOverImage;
+	private static BufferedImage winImage;
+	private static JButton exitButton = new JButton("EXIT");
+	private static JButton tryAgainButton = new JButton("Try again");
+	private static JPanel finalButtonPanel = new JPanel();
+
 	public static void start() {
 		initAllComp();
-		frame.getContentPane().add(mainPanel, BorderLayout.CENTER);
-		frame.pack();
 		panelButtonCreateAndChoose.add(createButton, new GridBagConstraints(0, 0, 1, 1, 1, 0,
 				GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
-				new Insets(10, 10 , 10, 10), 0, 0 ));
+				new Insets(10, 10 , 10, 5), 10, 0 ));
 		panelButtonCreateAndChoose.add(chooseButton, new GridBagConstraints(1, 0, 1, 1, 1, 0,
 				GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
-				new Insets(10, 10 , 10, 10), 0, 0 ));
+				new Insets(10, 5 , 10, 10), 0, 0 ));
 		mainPanel.add(panelButtonCreateAndChoose, new GridBagConstraints(0, 4, 1, 1, 1, 0,
 				GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
 				new Insets(10, 150 , 10, 150), 0, 0 ));
-		mainPanel.setVisible(true);
+		frame.getContentPane().add(mainPanel, BorderLayout.CENTER);
+		frame.pack();
 		createHero();
 		chooseHero();
+	}
+
+	private static void initAllComp() {
+		frame = newWindow();
+		createButton = new JButton("Create");
+		chooseButton = new JButton("Choose");
+		mainPanel = new ImagePanel(new ImageIcon(Glob.PIC + "main.png").getImage(), frame.getWidth(), frame.getHeight());
+		panelMap = new ImagePanel(new ImageIcon(Glob.PIC + "map.png").getImage(), frame.getWidth(), frame.getHeight());
+		mainPanel.setLayout(new GridBagLayout());
+		panelButtonCreateAndChoose.setLayout(new GridBagLayout());
+		panelButtonCreateAndChoose.setBackground(new Color(0,0,0,200));
+
+		createButton.addActionListener(new ButtonAction());
+		createButton.setBorderPainted(false);
+		createButton.setBackground(new Color(0, 0, 0, 0));
+		createButton.setForeground(Color.WHITE);
+		chooseButton.addActionListener(new ButtonAction());
+		chooseButton.setBorderPainted(false);
+		chooseButton.setBackground(new Color(0, 0, 0, 0));
+		chooseButton.setForeground(Color.WHITE);
+
+		panelFieldCreate.setBackground(new Color(0,0,0,200));					//init create hero field
+		panelFieldCreate.setLayout(new GridBagLayout());
+
+		textPane.setEditable(false);														//init and add text about Default stat Heroes
+		textPane.setVisible(false);
+		textPane.setBackground(new Color(0, 0, 0, 0));
+		doc = textPane.getStyledDocument();
+		SimpleAttributeSet center = new SimpleAttributeSet();
+		StyleConstants.setAlignment(center, StyleConstants.ALIGN_JUSTIFIED);
+		StyleConstants.setForeground(center, Color.WHITE);
+		doc.setParagraphAttributes(0, doc.getLength(), center, true);
+		panelFieldCreate.add(textPane, new GridBagConstraints(0, 3, 2, 1, 1, 0,
+				GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
+				new Insets(10, 10 , 10, 10), 0, 0 ));
 	}
 
 	public static class CreateStartMap extends Thread {
@@ -100,20 +143,50 @@ public class Gui {
 	public static void createHeroInfoBar() {
 		heroInfoPanel.setLayout(new BoxLayout(heroInfoPanel, BoxLayout.Y_AXIS));
 		heroInfoPanel.setBackground(new Color(0, 0, 0, 0));
-		progressHP.setForeground(Color.RED);
+		progressHP.setForeground(new Color(128, 21, 21, 150));
 		progressHP.setMaximum(hero.getHP());
 		progressHP.setMinimum(0);
 		progressHP.setValue(hero.getHP());
 		progressHP.setStringPainted(true);
+		progressHP.setBorderPainted(false);
+		progressLevel.setForeground(new Color(13, 77, 77, 150));
 		progressLevel.setMaximum(hero.getMustLevel());
 		progressLevel.setMinimum(hero.getExp());
 		progressLevel.setValue(hero.getExp());
 		progressLevel.setStringPainted(true);
+		progressLevel.setBorderPainted(false);
 		heroInfoPanel.add(progressHP);
 		heroInfoPanel.add(progressLevel);
 		progressLevel.setAlignmentX(JComponent.CENTER_ALIGNMENT);
 		progressHP.setAlignmentX(JComponent.CENTER_ALIGNMENT);
 		frame.getContentPane().add(heroInfoPanel, BorderLayout.PAGE_START);
+	}
+
+	public static void finalView() {
+		panelMap.setVisible(false);
+		heroInfoPanel.setVisible(false);
+		gameOverPanel = new ImagePanel(new ImageIcon(Glob.PIC + "die.png").getImage(), frame.getWidth(), frame.getHeight());
+		frame.getContentPane().add(gameOverPanel, BorderLayout.CENTER);
+		frame.pack();
+		tryAgainButton.addActionListener(new finalButtomAction());
+		exitButton.addActionListener(new finalButtomAction());
+		tryAgainButton.setBackground(new Color(0, 0, 0, 0));
+		tryAgainButton.setBorderPainted(false);
+		exitButton.setBorderPainted(false);
+		exitButton.setBackground(new Color(0, 0, 0, 0));
+		finalButtonPanel.setLayout(new GridBagLayout());
+		finalButtonPanel.setBackground(new Color(255, 255, 255, 100));
+		finalButtonPanel.add(tryAgainButton, new GridBagConstraints(0, 0, 1, 1, 1, 0,
+				GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
+				new Insets(10, 10 , 10, 5), 0, 0 ));
+		finalButtonPanel.add(exitButton, new GridBagConstraints(1, 0, 1, 1, 1, 0,
+				GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
+				new Insets(10, 5 , 10, 10), 30, 0 ));
+		gameOverPanel.setLayout(new GridBagLayout());
+		gameOverPanel.add(finalButtonPanel, new GridBagConstraints(0, 0, 1, 1, 1, 0,
+				GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
+				new Insets(525, 150 , 10, 150), 0, 0 ));
+		gameOverPanel.setVisible(true);
 	}
 
 	public static void updateMap() {
@@ -160,32 +233,6 @@ public class Gui {
 		return null;
 	}
 
-	private static void initAllComp() {
-		frame = newWindow();
-		mainPanel = new ImagePanel(new ImageIcon(Glob.PIC + "main.png").getImage(), frame.getWidth(), frame.getHeight());
-		panelMap = new ImagePanel(new ImageIcon(Glob.PIC + "map.png").getImage(), frame.getWidth(), frame.getHeight());
-		mainPanel.setLayout(new GridBagLayout());
-		panelButtonCreateAndChoose.setLayout(new GridBagLayout());							//two button create and choose
-		panelButtonCreateAndChoose.setBackground(new Color(0,0,0,200));
-		createButton.addActionListener(new ButtonAction());									//add Listener
-		chooseButton.addActionListener(new ButtonAction());
-
-		panelFieldCreate.setBackground(new Color(0,0,0,200));					//init create hero field
-		panelFieldCreate.setLayout(new GridBagLayout());
-
-		textPane.setEditable(false);														//init and add text about Default stat Heroes
-		textPane.setVisible(false);
-		textPane.setBackground(new Color(0, 0, 0, 0));
-		doc = textPane.getStyledDocument();
-		SimpleAttributeSet center = new SimpleAttributeSet();
-		StyleConstants.setAlignment(center, StyleConstants.ALIGN_JUSTIFIED);
-		StyleConstants.setForeground(center, Color.WHITE);
-		doc.setParagraphAttributes(0, doc.getLength(), center, true);
-		panelFieldCreate.add(textPane, new GridBagConstraints(0, 3, 2, 1, 1, 0,
-				GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
-				new Insets(10, 10 , 10, 10), 0, 0 ));
-	}
-
 	private static void createHero() {
 		String[] classHero = {"Human", "Orc", "Elf", "Dwarf"};
 		JComboBox<String> comboBox = new JComboBox<>(classHero);
@@ -215,9 +262,12 @@ public class Gui {
 	}
 
 	private static void chooseHero() {
+		if (tableModel != null) {
+			panelTableChoose.remove(scrollPane);
+		}
 		tableModel = new MyTableModel(heroManager.getAllTarget());					//init choose  heroes table
 		allHeroesTable = new JTable(tableModel);
-		JScrollPane scrollPane = new JScrollPane(allHeroesTable);
+		scrollPane = new JScrollPane(allHeroesTable);
 		scrollPane.setPreferredSize(new Dimension(300, 300));
 		panelTableChoose.setLayout(new BorderLayout());
 		panelTableChoose.add(scrollPane, BorderLayout.CENTER);
@@ -313,6 +363,8 @@ public class Gui {
 					default:
 						break;
 				}
+			} else {
+				finalView();
 			}
 			progressHP.setValue(hero.getHP());
 			progressLevel.setValue(hero.getExp());
@@ -376,6 +428,25 @@ public class Gui {
 		}
 	}
 
+
+	public static class finalButtomAction implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (((JButton) e.getSource()).getText().equals("EXIT")) {
+				Message.goodBye();
+			} else {
+				//frame.dispose();
+				hero = null;
+				//start();
+				panelMap.setVisible(false);
+				gameOverPanel.setVisible(false);
+				createButton.setText("Create");
+				mainPanel.setVisible(true);
+				panelButtonCreateAndChoose.setVisible(true);
+			}
+		}
+	}
+
 	public static class ButtonAction implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
@@ -400,15 +471,20 @@ public class Gui {
 						panelFieldCreate.setVisible(false);
 					}
 				} else if (((JButton) e.getSource()).getText().equals("Choose")){
+					chooseHero();
 					createButton.setText("Start");
-					tableModel.setHeroes(heroManager.getAllTarget());
-					tableModel.fireTableDataChanged();
+					//tableModel.setHeroes(heroManager.getAllTarget());
+					//tableModel.fireTableDataChanged();
 					panelFieldCreate.setVisible(false);
 					panelTableChoose.setVisible(true);
 				} else if (((JButton) e.getSource()).getText().equals("Start")){
-					panelTableChoose.setVisible(false);
-					panelButtonCreateAndChoose.setVisible(false);
-					createMap.run();
+					if (hero != null) {
+						panelTableChoose.setVisible(false);
+						panelButtonCreateAndChoose.setVisible(false);
+						createMap.run();
+					} else {
+						JOptionPane.showMessageDialog(null, "Choose a hero", "Warning", JOptionPane.WARNING_MESSAGE);
+					}
 				}
 			} else if (e.getSource() instanceof JComboBox) {
 				JComboBox box = (JComboBox)e.getSource();
